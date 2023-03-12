b3901e6f7c9e1d2f56fdafbce98b2305a604d6dd
Sommai Krangpanich <sommai.k@gmail.com>
2023-03-12 09:49:24 +0000

    update code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,42 @@
-import { FC, useState } from "react";
+import { FC, useState, useEffect } from "react";
+
+interface GeoModel {
+  lat: string;
+  lng: string;
+}
+
+interface AddressModel {
+  street: string;
+  geo: GeoModel;
+}
+
+interface UserModel {
+  name: string;
+  username: string;
+  address: AddressModel;
+}
 
 const Home: FC = () => {
-  const [users, setUsers] = useState([]);
+  // hook
+  const [users, setUsers] = useState<UserModel[]>([]);
+  const [headers, setHeaders] = useState<string[]>(["name", "username"]);
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  // function
   const loadData = () => {
     fetch("https://jsonplaceholder.typicode.com/users").then((resp) => {
-      resp.json().then((data: []) => {
+      resp.json().then((data: UserModel[]) => {
         setUsers(data);
+        if (data.length > 0) {
+          const d = data[0];
+          const cols: string[] = [];
+          for (let k in d) {
+            cols.push(k);
+          }
+          setHeaders(cols);
+        }
       });
     });
   };
@@ -13,12 +44,17 @@ const Home: FC = () => {
     <>
       <button onClick={loadData}>Load</button>
       <table>
+        <thead>
+          {headers.map((value) => (
+            <th>{value}</th>
+          ))}
+        </thead>
         <tbody>
           {users.map((value, index) => {
             return (
               <tr key={index}>
-                <td>{value["name"]}</td>
-                <td>{value["username"]}</td>
+                <td>{value.name}</td>
+                <td>{value.username}</td>
               </tr>
             );
           })}
